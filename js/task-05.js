de7e57b5f525cb869786136ed55342797e169529
Yevhen Tsapko <evgentsa@gmail.com>
2023-01-27 21:08:23 +0200

    "Anonymus" is fixed

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,7 @@ const outputGreting = document.querySelector("span#name-output");
 const inputName = document.querySelector("input, #name-output");
 
 inputName.addEventListener("input", (event) => {
-  outputGreting.textContent = event.currentTarget.value;
+  outputGreting.textContent = event.currentTarget.value
+    ? (outputGreting.textContent = event.currentTarget.value)
+    : (outputGreting.textContent = "Anonymous");
 });
